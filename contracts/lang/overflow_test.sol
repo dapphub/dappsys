81be0370387d66755a224520b28569a0adc9ee99
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2015-10-21 16:44:58 -0400

    overflow test logs

--- contracts/lang/overflow_test.sol
+++ contracts/lang/overflow_test.sol
@@ -4,21 +4,13 @@ import 'core/test.sol';
 
 contract OverflowTest is Test {
     function helper(uint depth) external returns (bytes32 val, bool ok) {
-        logs("in helper");
         bytes32 magic = 0x42;
         if( depth == 109 ) {
             return (magic, true);
         }
         var (ret, is_ok) = this.helper(depth+1);
-/*
-        if( depth == 5 ) {
-            assertFalse( is_ok );
-            assertEq32( ret, 0x0 );
-        } else {
-            assertTrue( is_ok );
-            assertEq32( ret, magic );
-        }
-*/
+        log_uint(depth);
+        log_bytes32(ret);
         return (magic, true);
     }
     function testOverflow() {
