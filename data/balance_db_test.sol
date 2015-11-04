c9e64df82719f746d955d2d91849f413c96db445
nikolai <nikolai@makerdao.com>
2015-11-04 18:12:10 +0000

    add overflow test

--- data/balance_db_test.sol
+++ data/balance_db_test.sol
@@ -23,12 +23,15 @@ contract DSBalanceDB_Test is Test {
     }
     function testSubBalanceFailsBelowZero() tests("sub_balance") {
         bool ok = db.sub_balance(me, 100);
-//        assertTrue(ok, "sub failed below zero");
-//        db.add_balance(me, 50);
- //       ok = db.sub_balance(me, 51);
         this.assertFalse(ok, "sub failed below zero");
     }
-
+    function testAddBalanceFailsAboveOverflow() tests("add_balance") {
+	db.add_balance(bob, 2**256-5);
+	var ok = db.add_balance(bob, 4);
+	assertTrue(ok);
+	ok = db.add_balance(bob, 1);
+	assertFalse(ok);
+    }
     function testAdminTransfer() {
         bool ok = db.add_balance(bob, 100);
         ok = db.move_balance(bob, me, 40);
