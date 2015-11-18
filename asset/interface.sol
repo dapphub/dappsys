c9b994921f657907cfffddd98c42a59161e68c59
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2015-11-18 11:38:43 -0500

    DSAsset1 interface

--- asset/interface.sol
+++ asset/interface.sol
@@ -1,3 +1,4 @@
+// This interface is a minimum viable asset controller.
 contract DSAsset0 // is DSAuth
 {
     function get_supply() constant returns (uint balance, bool ok);
@@ -20,20 +21,32 @@ contract DSAsset0 // is DSAuth
     event event_swap_db( address db, address old_db_new_owner );
 }
 
+// This interface adds small set of functions for contract-based asset manipulation.
+// It is a subset of old the MakerAsset and the proposed standard asset API in the ethereum wiki.
+contract DSAsset1 // is DSAuth
+{
+    function transfer( address to, uint amount) returns (bool ok);
+    function transfer_and_notify( address to
+                                , uint amount
+                                , bytes32 data );
+    function transfer_and_notify( address from
+                                , address to
+                                , uint amount
+                                , bytes32 data );
+    function transferFrom( address from, address to, uint amount ) returns (bool ok);
+    function balanceOf( address who ) constant returns (uint amount, bool ok);
+
+    event event_transfer_and_notify( address indexed from
+                                   , address indexed to
+                                   , bytes32 indexed data
+                                   , uint amount);
 
 
 
 
-contract AssetPatterns {
+    function approve( address who ) returns (bool ok);
+    function unapprove( address who ) returns (bool ok);
+    function isApprovedFor( address holder, address spender ) constant returns (bool approved, bool ok);
 
-    // The basic asset pipeline tool. Explicitly passes permission
-    // to take control of the asset from sender to receipient.
-    //function approve( address who, uint amount ) returns (bool ok);
-    //function transfer_from( address who, address to, uint amount) returns (bool ok);
-
-    // increase approved amount for everyone in this transaction
-    //function withdraw
-    // take this much from the transaction
-    //function charge
-    
+    event event_updated_approval( address indexed holder, address indexed spender, bool indexed can_spend );
 }
