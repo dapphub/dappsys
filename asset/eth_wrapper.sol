4a152e972ef90dbed6099fc5cf84f41ec3747187
nikolai <nikolai@makerdao.com>
2015-11-21 06:33:31 +0000

    fix build

--- asset/eth_wrapper.sol
+++ asset/eth_wrapper.sol
@@ -10,13 +10,13 @@ contract EthManager0 is DSAsset0 {}
 contract EthManager0Impl is DSAsset0Impl {
     // TODO double-check .call() semantics to ensure balance is always right
     function withdraw_eth( uint amount ) returns (bool ok) {
-        ok = db.sub_balances( msg.sender, amount );
+        ok = db.sub_balance( msg.sender, amount );
         if( ok ) {
             msg.sender.call.value(amount)();
         }
         return ok;
     }
     function() returns (bool) {
-        return db.add_balances( msg.sender, msg.value );
+        return db.add_balance( msg.sender, msg.value );
     }
 }
