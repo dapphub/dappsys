b8aeee87f204a85b9f2b081ee6679265ee705bfb
nikolai <nikolai@makerdao.com>
2015-11-21 17:55:45 +0000

    signature behavior test

--- src/sol/lang/functypes_test.sol
+++ src/sol/lang/functypes_test.sol
@@ -2,31 +2,35 @@ import 'dapple/test.sol';
 
 contract WithIdentifier {
     bool public transferred;
+    bool public missed;
     function transfer( address to, uint amount, bytes32 id ) returns (bool ok) {
         transferred = true;
         return true;
     }
+    function() {
+        missed = true;
+    }
 }
 
 contract NoIdentifier {
-    bool public transferred;
-    function transfer( address to, uint amount ) returns (bool ok) {
-        transferred = true;
-        return true;
-    }
+    function transfer( address to, uint amount ) returns (bool ok);
 }
 
 
 contract SignatureBehaviorTest is Test {
     WithIdentifier wid;
-    NoIdentifier   nid;
     function setUp() {
         wid = new WithIdentifier();
-        nid = new NoIdentifier();
     }
-    function testExcludingID() {
-        var ok = NoIdentifier(wid).transfer( address(this), 100 );
-        assertTrue( ok );
+    function testHypothesis() {
+        NoIdentifier(wid).transfer( address(this), 100 );
+        assertTrue( wid.transferred() );
+    }
+    function testReality() {
+        NoIdentifier(wid).transfer(address(this), 100);
+        assertTrue( wid.missed() );
+        assertFalse( wid.transferred() );
+        wid.transfer( address(this), 100, "identifier" );
         assertTrue( wid.transferred() );
     }
 }
