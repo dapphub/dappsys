b54bb7767e4d6ad1608444a8e8af23c1d5d2e436
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2015-12-27 15:46:46 -0500

    fix base actor test 2

--- src/sol/actor/base_actor_test.sol
+++ src/sol/actor/base_actor_test.sol
@@ -3,15 +3,20 @@ import 'dapple/test.sol';
 import 'dapple/debug.sol';
 
 contract CallReceiver is Debug {
-    bytes public last_calldata;
-    uint public last_value;
-    // You can't pass `bytes` around yet...
-    function lastCalldataPrefix() returns (byte[4]) {
-        byte[4] memory b;
-        for( var i = 0; i < 4 && i < last_calldata.length; i++ ) {
-            b[i] = last_calldata[i];
+    bytes last_calldata;
+    uint last_value;
+    function compareLastCalldata( bytes data ) returns (bool) {
+        // last_calldata.length might be longer because calldata is padded
+        // to be a multiple of the word size
+        if( data.length > last_calldata.length ) {
+            return false;
         }
-        return b;
+        for( var i = 0; i < data.length; i++ ) {
+            if( data[i] != last_calldata[i] ) {
+                return false;
+            }
+        }
+        return true;
     }
     function() {
         last_calldata = msg.data;
@@ -29,20 +34,14 @@ contract DSBaseActorTest is Test {
         cr = new CallReceiver();
     }
     function testProxyCall() {
-        byte[4] memory prefix;
-        calldata.push(0x0);
-        calldata.push(0x1);
-        calldata.push(0x2);
-        calldata.push(0x3);
-        prefix[0] = 0x0;
-        prefix[1] = 0x1;
-        prefix[2] = 0x2;
-        prefix[3] = 0x3;
+        calldata.push(byte(0x0));
+        calldata.push(byte(0x1));
+        calldata.push(byte(0x2));
+        calldata.push(byte(0x3));
+        calldata.push(byte(0x4));
 
         a.execute( address(cr), calldata, 0, 0 );
-        var last = cr.lastCalldataPrefix();
-        for( var i = 0; i < 4; i++ ) {
-            assertTrue( last[i] == prefix[i] );
-        }
+        var length = cr.compareLastCalldata( calldata );
+        assertTrue( cr.compareLastCalldata( calldata ) );
     }
 }
