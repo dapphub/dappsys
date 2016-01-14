39ef1ed92239d5458897b7ccb21dd8dd57f112e0
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2016-01-14 17:30:50 -0500

    use DSAuth for multisig

--- src/sol/gov/easy_multisig.sol
+++ src/sol/gov/easy_multisig.sol
@@ -13,18 +13,18 @@ import 'dapple/debug.sol';
  * where calldata is correctly formatted for `TargetType(target).doAction(arg1, arg2)`
  */
 contract DSEasyMultisig is DSBaseActor
+                         , DSAuth
 {
     // How many confirmations an action needs to execute.
     uint _required;
     // How many members this multisig has.
     uint _member_count;
+    // Auto-locks once this reaches zero.
+    uint _members_remaining;
     // Maximum time between proposal time and trigger time.
     uint _expiration;
     uint _last_action_id;
 
-    // temporary setup storage
-    address _setup_authority;
-    uint _setup_members_added;
 
     struct action {
         address target;
@@ -45,42 +45,42 @@ contract DSEasyMultisig is DSBaseActor
     mapping( address => bytes ) easy_calldata;
     // Can this address add confirmations?
     mapping( address => bool ) is_member;
-    
+   
+    event MemberAdded( address who ); 
     event Proposed( uint action_id );
     event Confirmed( uint action_id, address who );
     event Triggered( uint action_id, bool result );
 
     function DSEasyMultisig( uint required, uint member_count, uint expiration ) {
-        // `_setup_authority` is the address which is allowed to instantiate the initial members.
-        // It is like a temporary owner address.
-        _setup_authority = msg.sender;
         _required = required;
         _member_count = member_count;
+        _members_remaining = member_count;
         _expiration = expiration;
     }
     // The setup authority can add members until they reach `member_count`, after which the
-    // contract is finalized (`_setup_authority` set to address 0).
-    function addMember( address who ) returns (bool)
+    // contract is finalized (`_authority` is set to 0).
+    function addMember( address who ) auth() returns (bool)
     {
-        if( msg.sender != _setup_authority ) {
-            return false;
-        }
         if( is_member[who] ) {
             return false;
         }
         is_member[who] = true;
-        _setup_members_added++;
-        if( _setup_members_added == _member_count ) {
-            delete _setup_authority;
-            delete _setup_members_added;
+        MemberAdded(who);
+        _members_remaining--;
+        if( _members_remaining == 0 ) {
+            _authority = address(0x0);
+            _auth_mode = false;
         }
         return true;
+    }
+    function isMember( address who ) constant returns (bool) {
+        return is_member[who];
     }
 
     // Some constant getters
     function getInfo()
              constant
-             returns (uint required, uint members, uint expiration, uint last_proposed_action)
+             returns ( uint required, uint members, uint expiration, uint last_proposed_action)
     {
         return (_required, _member_count, _expiration, _last_action_id);
     }
@@ -92,10 +92,6 @@ contract DSEasyMultisig is DSBaseActor
         var a = actions[action_id];
         return (a.confirmations, a.expiration, a.triggered, a.result);
     }
-    function isMember( address who ) constant returns (bool) {
-        return is_member[who];
-    }
-
     // `propose` an action using the calldata from this sender's last call.
     function easyPropose( address target, uint value, uint gas ) returns (uint action_id) {
         return propose( target, easy_calldata[msg.sender], value, gas );
