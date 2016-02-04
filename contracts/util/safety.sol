eccc9a474fe68d61e73f2101b7ea7a74ebe9ffd3
Nikolai Mushegian <nikolai@bitshares.org>
2016-02-04 20:35:41 +0000

    add DSSafeAddSub

---
+++ contracts/util/safety.sol
@@ -0,0 +1,17 @@
+contract DSSafeAddSub {
+    function safeToAdd(uint a, uint b) internal returns (bool) {
+        return (a + b > a);
+    }
+    function safeAdd(uint a, uint b) internal returns (uint) {
+        if (!safeToAdd(a, b)) throw;
+        return a + b;
+    }
+
+    function safeToSubtract(uint a, uint b) internal returns (bool) {
+        return (b <= a);
+    }
+    function safeSub(uint a, uint b) internal returns (uint) {
+        if (!safeToSubtract(a, b)) throw;
+        return a - b;
+    } 
+}
