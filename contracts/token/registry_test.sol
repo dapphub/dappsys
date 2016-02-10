2f4606e1a0aea85ed585d605a4c38e770f27cddf
Ryan Casey <ryepdx@gmail.com>
2016-02-10 21:41:19 +0000

    Add test for DSTokenRegistry.

--- contracts/token/registry_test.sol
+++ contracts/token/registry_test.sol
@@ -1,1 +1,21 @@
+import 'dapple/test.sol';
 import 'token/registry.sol';
+import 'token/base.sol';
+
+contract TokenRegistryTest is Test {
+    uint constant issuedAmount = 1000;
+
+    DSTokenBase token;
+    DSTokenRegistry registry;
+
+    function setUp() {
+        registry = new DSTokenRegistry();
+        token = new DSTokenBase(issuedAmount);
+    }
+
+    function testGetToken() {
+        bytes32 tokenName = "Kanye Coin";
+        registry.set(tokenName, bytes32(address(token)));
+        assertEq(registry.getToken(tokenName), token);
+    }
+}
