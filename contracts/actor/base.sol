64ded8e1e2645da3619ee86494fe00cd26467a5d
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2016-04-04 13:36:53 -0400

    remove redundant base_actor.sol

--- contracts/actor/base.sol
+++ contracts/actor/base.sol
@@ -1,2 +1,20 @@
-// Filename alias.
-import 'actor/base_actor.sol';
+// A base contract mostly used by governance contracts in `gov`.
+// For now, this just means the multisig contract, but it could
+// be used for stake-vote or futarchy.
+contract DSBaseActor {
+    // return result of `call` keyword
+    function tryExec( address target, bytes calldata, uint value)
+             internal
+             returns (bool call_ret)
+    {
+        return target.call.value(value)(calldata);
+    }
+    function exec( address target, bytes calldata, uint value)
+             internal
+    {
+        if(!tryExec(target, calldata, value)) {
+            throw;
+        }
+    }
+}
+
