a916e16923d1163d2d917bcfff36ac923ee5e7f5
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2016-04-12 10:49:29 -0400

    vote db wip

--- contracts/data/vote_db.sol
+++ contracts/data/vote_db.sol
@@ -3,6 +3,22 @@
 import 'auth.sol';
 
 contract DSVoteDB is DSAuth {
+    uint issue_id;
+    uint proposal_id;
+    struct IPFSHash {
+        bytes20 first;
+        bytes20 second;
+    }
+    struct Proposal {
+        uint issue_id;
+        uint proposal_id;
+        bytes8 topic;
+        address owner;
+        uint vote;
+        uint16 expiration;
+    }
+    mapping( bytes8 => IPFSHash ) topics;
+    mapping( address => mapping( bytes8 => Ballot ) ) ballots;
 }
 
 // anyone can post a bond to propose a proposal.
