354f45765b76e23a728896e521fa2e5aa99fbacc
Ryan Casey <ryepdx@gmail.com>
2016-05-24 21:10:59 +0000

    Rename to DSTokenSupplyManager.

--- contracts/token/supply_manager_test.sol
+++ contracts/token/supply_manager_test.sol
@@ -2,52 +2,52 @@ import 'dapple/test.sol';
 
 import 'auth/basic_authority.sol';
 import 'data/balance_db.sol';
-import 'token/supply_controller.sol';
+import 'token/supply_manager.sol';
 
-contract DSTokenSupplyControllerTest is Test, DSAuthUser {
+contract DSTokenSupplyManagerTest is Test, DSAuthUser {
     DSBalanceDB db;
     DSBasicAuthority authority;
-    DSTokenSupplyController controller;
-    function DSTokenSupplyControllerTest() {
+    DSTokenSupplyManager manager;
+    function DSTokenSupplyManagerTest() {
         authority = new DSBasicAuthority();
     }
 
     function setUp() {
         db = new DSBalanceDB();
-        controller = new DSTokenSupplyController(db);
+        manager = new DSTokenSupplyManager(db);
         db.updateAuthority(authority, DSAuthModes.Authority);
         authority.setCanCall(
-          controller, db, bytes4(sha3('addBalance(address,uint256)')), true);
+          manager, db, bytes4(sha3('addBalance(address,uint256)')), true);
         authority.setCanCall(
-          controller, db, bytes4(sha3('subBalance(address,uint256)')), true);
+          manager, db, bytes4(sha3('subBalance(address,uint256)')), true);
 
-        controller.updateAuthority(authority, DSAuthModes.Authority);
+        manager.updateAuthority(authority, DSAuthModes.Authority);
         authority.setCanCall(
-          this, controller, bytes4(sha3('demand(uint256)')), true);
+          this, manager, bytes4(sha3('demand(uint256)')), true);
         authority.setCanCall(
-          this, controller, bytes4(sha3('destroy(uint256)')), true);
+          this, manager, bytes4(sha3('destroy(uint256)')), true);
     }
 
     function testDemand() {
       assertEq(db.getBalance(this), 0);
-      controller.demand(10);
+      manager.demand(10);
       assertEq(db.getBalance(this), 10);
       assertEq(db.getSupply(), 10);
     }
 
     function testDestroy() {
       assertEq(db.getBalance(this), 0);
-      assertEq(db.getBalance(controller), 0);
+      assertEq(db.getBalance(manager), 0);
       assertEq(db.getSupply(), 0);
 
-      controller.demand(10);
+      manager.demand(10);
       assertEq(db.getBalance(this), 10);
-      assertEq(db.getBalance(controller), 0);
+      assertEq(db.getBalance(manager), 0);
       assertEq(db.getSupply(), 10);
 
-      controller.destroy(9);
+      manager.destroy(9);
       assertEq(db.getBalance(this), 1);
-      assertEq(db.getBalance(controller), 0);
+      assertEq(db.getBalance(manager), 0);
       assertEq(db.getSupply(), 1);
     }
 }
