45cab58114b3af571002e1d11f48660199586dcd
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2016-06-01 17:26:37 -0400

    move components to dappsys

---
+++ contracts/component.sol
@@ -0,0 +1,19 @@
+import 'auth.sol';
+import 'data/nullmap.sol';
+
+contract DSComponent is DSAuth {
+    DSNullMap _env;
+    
+    function DSComponent(DSNullMap env) {
+        updateEnvironment(env);
+    }
+    function updateEnvironment(DSNullMap env)
+        auth
+    {
+        _env = env;
+        refreshEnvironment();
+    }
+    // Save references to local storage.
+    // System updates will call this.
+    function refreshEnvironment();
+}
