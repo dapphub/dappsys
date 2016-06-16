2f93b9e4c5cfc2991d53b57c20c7731746e6d1f0
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2016-06-15 21:07:05 -0400

    enable transferFrom on vault

--- contracts/token/vault.sol
+++ contracts/token/vault.sol
@@ -10,14 +10,10 @@ contract DSTokenVault is DSAuth {
     {
         return token.transfer(to, amount);
     }
-
-/*   Do we need this? This would allow people to `approve` the vault and let any
-     vault-authorized contract to spend. This would be more like a "token router" than a vault.
     function transferFrom(ERC20 token, address from, address to, uint amount)
         auth
         returns (bool)
     {
         return token.transferFrom(from, to, amount);
     }
-*/
 }
