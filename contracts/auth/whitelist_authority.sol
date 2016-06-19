6735723081949fcfc7cd720c7008218e8096ed6a
Daniel Brockman <daniel@brockman.se>
2016-06-19 14:35:02 +0200

    DSWhitelistAuthority is DSAuthority

--- contracts/auth/whitelist_authority.sol
+++ contracts/auth/whitelist_authority.sol
@@ -1,10 +1,14 @@
 import 'auth.sol';
 
-contract DSWhitelistAuthority is DSAuth {
-    mapping(address=>bool) _is_whitelisted;
-    function canCall(address caller, address code, bytes4 sig) returns (bool) {
+contract DSWhitelistAuthority is DSAuthority, DSAuth {
+    mapping(address => bool) _is_whitelisted;
+
+    function canCall(
+        address caller, address code, bytes4 sig
+    ) returns (bool) {
         return _is_whitelisted[caller];
     }
+
     function setIsWhitelisted(address who, bool is_whitelisted)
         auth
     {
