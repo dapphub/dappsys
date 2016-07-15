2a4936e00f7861045c3165a8e835153ea2cc9b7a
Daniel Brockman <daniel@brockman.se>
2016-07-15 18:16:35 +0200

    ASSERT -> assert

--- contracts/base.sol
+++ contracts/base.sol
@@ -1,26 +1,15 @@
 contract DSBase {
-    function ASSERT(bool what)
-        internal
-    {
-        if (NOT(what)) {
-            throw;
-        }
+    function assert(bool condition) internal {
+        if (!condition) throw;
     }
-    function NOT(bool what)
-        internal
-        constant
-        returns (bool)
-    {
-        return !what;
-    }
-    function throws(bytes32 reason)
-        internal
-    {
+
+    function throws(bytes32 reason) internal {
         log1(msg.sig, reason);
         throw;
     }
-    // eventually you'll be able to override fallbacks
-    function() {
+
+    // TODO: eventually you'll be able to override fallbacks
+    function () {
         throw;
     }
 }
